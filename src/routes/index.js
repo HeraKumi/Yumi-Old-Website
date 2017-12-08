9f72f357059a9233c060e12dacfc76d703276193
Hera <spacebarbutton123@gmail.com>
2017-12-08 10:04:49 -0700

    added alot of file and updated it to match my discord bot

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+ isOnline = require('is-online');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/discord', function(req, res, next) {
+  res.render('discord', { title: 'Yumi' });
+});
+
+router.get('/commands', function(req, res, next) {
+  res.render('commands', { title: 'Commands' });
+});
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq', { title: 'FAQ' });
 });
 
 module.exports = router;
