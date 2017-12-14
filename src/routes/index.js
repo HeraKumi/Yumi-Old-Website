0a33496f5922ff1eba82096e86212bdd49d0c6f2
Hera Kumi <spacebarbutton123@gmail.com>
2017-12-14 02:50:29 -0700

    Moved Commands to its own page

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,16 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-router.get('/discord', function(req, res, next) {
-  res.render('discord', { title: 'Yumi' });
-});
-
 router.get('/commands', function(req, res, next) {
   res.render('commands', { title: 'Commands' });
-});
-
-router.get('/faq', function(req, res, next) {
-  res.render('faq', { title: 'FAQ' });
 });
 
 module.exports = router;
